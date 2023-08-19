e2d535ccdc29415ff8dac9d4456aae2804f35a1d
Kang-xiao-zhuang <2247830091@qq.com>
2023-08-19 13:40:12 +0800

    project init

---
+++ heima-leadnews-service/heima-leadnews-user/src/main/java/com/heima/user/UserApplication.java
@@ -0,0 +1,18 @@
+package com.heima.user;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+
+@EnableDiscoveryClient
+@MapperScan("com.heima.user.mapper")
+@SpringBootApplication
+public class UserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserApplication.class,args);
+    }
+}
